f936964115ecf520bcd40767034479c84a548bbe
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-10-23 14:50:12 -0400

    Add tts.py skeleton and package.xml value

---
+++ src/tts.py
@@ -0,0 +1,9 @@
+# Script that handles the communication with the tts system
+#
+# Author: Maxime St-Pierre <me@maximest-pierre.me>
+
+import rospy
+import urllib2
+import os, time
+
+def google_tts()
